c231d8aef03e8a698378789ba8f89b5e34c8e8f8
ziadmohammed0 <zizo.alprnc.90@gmail.com>
2025-05-02 09:08:08 +0300

    initialized main branch project

---
+++ code/ESP32/esp32s_stm32f405xxx_rover/src/main.cpp
@@ -0,0 +1,27 @@
+/**
+ * @file main.cpp
+ * @author Ziad Mohammed Fathy
+ * @brief code of esp32s to connected with stm32f405rgt6
+ *        to add wifi for stm32 to control rover of robot.
+ * @version 0.1
+ * @date 2025-05-02
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+
+#include <Arduino.h>
+
+// put function declarations here:
+ 
+void setup() {
+  // put your setup code here, to run once:
+  
+}
+ 
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+ 
+// put function definitions here:
