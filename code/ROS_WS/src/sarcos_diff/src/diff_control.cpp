a4636514787ea245c4e8bffaf359e8227788fafc
Ziad Fathy <ziadfathy498@gmail.com>
2026-03-17 21:26:35 +0200

    Auther: Ziad, Breif: add ros workspace

---
+++ code/ROS_WS/src/sarcos_diff/src/diff_control.cpp
@@ -0,0 +1,10 @@
+#include <cstdio>
+
+int main(int argc, char ** argv)
+{
+  (void) argc;
+  (void) argv;
+
+  printf("hello world sarcos_diff package\n");
+  return 0;
+}
---
+++ code/ROS_WS/src/sarcos_nav/src/navigation.cpp
@@ -0,0 +1,10 @@
+#include <cstdio>
+
+int main(int argc, char ** argv)
+{
+  (void) argc;
+  (void) argv;
+
+  printf("hello world sarcos_nav package\n");
+  return 0;
+}
